d35119a8055c5372cf275d4e2104ed2a533a8270
A. M <xamurej@users.noreply.github.com>
2016-07-06 22:43:52 +0200

    added class skeleton example

---
+++ cli_app/_examples/skeleton_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""Module docstring.
+
+This serves as a long usage message.
+"""
+
+
+class YourClass(object):
+
+    def __init__(self):
+        pass
