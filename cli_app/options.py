7c91d2a69ad5abf1d55dc2a09979b57dfa1bf116
A. M <xamurej@users.noreply.github.com>
2016-07-14 23:12:24 +0200

    define options

--- cli_app/options.py
+++ cli_app/options.py
@@ -16,7 +16,7 @@ class Options(object):
 
     def __init__(self):
         self.parser = configargparse.ArgParser(
-            default_config_files=['/etc/settings.ini', '~/.my_settings'])
+            default_config_files=['./config.ini'])
 
         self.parser.add('-c', '--my-config',
                         required=False,
@@ -24,20 +24,11 @@ class Options(object):
                         help='config file path')
 
         # this option can be set in a config file because it starts with '--'
-        self.parser.add('--genome',
-                        required=True,
-                        help='path to genome file')
-
-        self.parser.add('-v',
-                        help='verbose',
-                        action='store_true')
-
-        # this option can be set in a config file because it starts with '--'
-        self.parser.add('-d', '--dbsnp',
-                        help='known variants .vcf',
-                        env_var='DBSNP_PATH')
-
-        #self.parser.add('vcf', nargs='+', help='variant file(s)')
+        self.parser.add('--text',
+                        required=False,
+                        help='text for output',
+                        default='Hello world',
+                        env_var='APP_TEXT')
 
     def parse(self):
         return self.parser.parse_args()
