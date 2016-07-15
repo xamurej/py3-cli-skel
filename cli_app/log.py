06604594d7f83de464c2ad8ddaab40d56d927254
A. M <xamurej@users.noreply.github.com>
2016-07-15 23:10:30 +0200

    added json logging

--- cli_app/log.py
+++ cli_app/log.py
@@ -8,30 +8,22 @@ This serves as a long usage message.
 import logging
 import datetime
 
-
-class MyFormatter(logging.Formatter):
-
-    converter = datetime.datetime.fromtimestamp
-
-    def formatTime(self, record, date_fmt=None):
-        ct = self.converter(record.created)
-        t = ct.strftime("%Y-%m-%dT%H:%M:%S")
-        s = "%s.%03dZ" % (t, record.msecs)
-        return s
+from pythonjsonlogger import jsonlogger
 
 
 class Logger(object):
-    FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
     LOGGERLIST = [
         'options',
         'main',
         'output',
     ]
 
+    FORMAT='%(asctime) %(module) %(levelname) %(message)'
+
     def __init__(self):
         self.handler = logging.StreamHandler()
 
-        self.formatter = MyFormatter(fmt=self.FORMAT)
+        self.formatter = jsonlogger.JsonFormatter(self.FORMAT)
 
         self.handler.setFormatter(self.formatter)
         self.handler.setLevel(logging.DEBUG)
